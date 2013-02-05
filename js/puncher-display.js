6de30cbcb66903fc9e3ce1308967b88e725d8e2a
HadrienMP <h.menspellen@gmail.com>
2013-02-05 18:41:40 +0100

    Amélioration de l'affichage

--- js/puncher-display.js
+++ js/puncher-display.js
@@ -12,6 +12,8 @@ function toggleCookieState() {
 	if (!$('#cookie-button').hasClass('cookie-active')) {
 		$('#cookie-button, #cookie-state').addClass('cookie-active', 1000, "easeInOutCubic");
 	} else {
+		// FIXME Hack to prevent the glow of the indicators button to show on the cookie button when closing
+		$('#cookie-button').removeClass('indicators-active');
 		$('#cookie-button, #cookie-state').removeClass('cookie-active', 1000, "easeInOutCubic");
 	}
 }
@@ -21,6 +23,8 @@ function toggleIndicatorsState() {
 	if (!$('#indicators-button').hasClass('indicators-active')) {
 		$('#indicators, #indicators-button').addClass('indicators-active', 1000, "easeInOutCubic");
 	} else {
+		// FIXME Hack to prevent the glow of the indicators button to show on the cookie button when closing
+		$('#cookie-button').addClass('indicators-active');
 		$('#indicators, #indicators-button').removeClass('indicators-active', 1000, "easeInOutCubic");
 	}
 }
