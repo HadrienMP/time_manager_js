8c5c8dc71cb47e6532932269f3b0e658e4b5e757
HadrienMP <h.menspellen@gmail.com>
2013-02-22 21:29:29 +0100

    Recentrage du bouton de pointage

--- js/puncher-display.js
+++ js/puncher-display.js
@@ -47,7 +47,8 @@ function centerPuncher() {
     $('#cookie-section').css('right', $('#puncher-container').width() / 2);
     $('#indicators-section').css('left', $('#puncher-container').width() / 2);
 
-    $('#puncher-section').css('left', Math.floor($('#puncher-container').width() / 2 - $('#puncher-section').width() / 2));
+		// FIXME shouldn't have to substract 2px to center the puncher section
+    $('#puncher-section').css('left', Math.floor($('#puncher-container').width() / 2 - $('#puncher-section').width() / 2) - 2);
 }
 
 function noTimeParametres() {
